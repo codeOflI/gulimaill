5fab576c789dd4b8a6ae3ab8da84fab9e73b6431
Mars Lee <MarsLee5@foxmail.com>
2022-05-16 23:12:45 +0800

    add nacos-configration spring-cloud-gateway

--- gulimall-coupon/src/main/java/com/codeofli/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/codeofli/gulimall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +26,31 @@ import com.codeofli.common.utils.R;
  * @email 1162314270@qq.com
  * @date 2022-05-15 19:36:09
  */
+
 @RestController
 @RequestMapping("coupon/coupon")
+@RefreshScope
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R test(){
+
+        return R.ok().put("name",name).put("age",age);
+    }
+
+    @RequestMapping("/member/list")
+    public R membercoupons(){
+        CouponEntity couponEntity = new CouponEntity();
+        couponEntity.setCouponName("满100减10");
+        return R.ok().put("coupons",Arrays.asList(couponEntity));
+    }
 
     /**
      * 列表
